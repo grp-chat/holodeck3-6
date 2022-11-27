63ee5a197e2e99859f3f33e0cb43e4fda6af9ae0
andrew <julieyeng.js@gmail.com>
2022-11-27 14:30:02 +0700

    push

---
+++ client/js/Missions.js
@@ -0,0 +1,14 @@
+const missions = {
+    "1": "Mission: Retreat to extraction area without getting caught!",
+    "2": "Mission: Capture the humans before they escape!",
+    "3": "Mission: Game is in progress.................",
+    // "4": "Mission 4: Please peform mission 4... Please peform mission 4... Please peform mission 4...",
+    // "5": "Mission 5: Please peform mission 5... Please peform mission 5... Please peform mission 5..."
+
+}
+
+const teamDetails = {
+    "1": {teamMission: missions[1], art: "👨‍🚀", abilities: "Invisibility", desc: "Human Covert Unit"},
+    "2": {teamMission: missions[2], art: "👽", abilities: "Capture", desc: "Alien Sentry Unit"},
+    "0": {teamMission: missions[3], art: "👽", abilities: "Invisibility; Blink; EMP Bomb", desc: "Alien Dragoon Unit"}
+};
